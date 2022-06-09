c472fe4e69f0308fafb6ad5687978b8c9f95b0fd
Joelle Clayton <joelleclayton@mac.com>
2022-06-09 18:29:23 -0400

    submitting changes to vending machine project

--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -1,7 +1,9 @@
 package com.techelevator;
 
+import com.techelevator.model.VendingMachine;
 import com.techelevator.view.Menu;
 
+import java.io.FileNotFoundException;
 import java.util.Map;
 
 public class VendingMachineCLI {
@@ -11,6 +13,9 @@ public class VendingMachineCLI {
 	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE };
 
 	private Menu menu;
+
+
+
 
 	public VendingMachineCLI(Menu menu) {
 		this.menu = menu;
@@ -28,7 +33,7 @@ public class VendingMachineCLI {
 		}
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		Menu menu = new Menu(System.in, System.out);
 		VendingMachineCLI cli = new VendingMachineCLI(menu);
 		cli.run();
--- src/main/java/com/techelevator/model/VendingMachine.java
+++ src/main/java/com/techelevator/model/VendingMachine.java
@@ -2,30 +2,41 @@ package com.techelevator.model;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class VendingMachine {
     private int availableFunds; //derived
     private Map<String, Object> inventoryMap;
-    private int amountDeposited;
     private int customerChange; //derived
 
 
-    public Map<String, Object> getInventoryMap() {
+    public VendingMachine(Map<String,Object> inventoryMap, int availableFunds, int customerChange) {
+        this.inventoryMap = inventoryMap;
+        this.availableFunds = 0;
+        this.customerChange = 0;
+    }
 
-
+    public Map<String, Object> getInventoryMap() throws FileNotFoundException {
+        inventoryMap = loadInventory();
         return inventoryMap;
     }
+
+    public int addFunds(int funds){
+        availableFunds += funds;
+        return availableFunds;
+    }
+
+    public List<Item> makePurchase() {
+        List<Item> purchased = new ArrayList<>();
+    }
+
 
     private Map<String, Object> loadInventory() throws FileNotFoundException {
 
         String filePath = "C:\\Users\\Student\\workspace\\mod-1-capstone-java-team-1\\vendingmachine.csv";
         File inputFile = new File(filePath);
 
-        Map<String, Object> inventoryMap = new HashMap<>();
+        Map<String, Object> inventoryMap = new TreeMap<>();
 
 
         try (Scanner fileScanner = new Scanner(inputFile)) {
@@ -60,6 +71,8 @@ public class VendingMachine {
             return inventoryMap;
 
         }
+
+
     }
 }
 
