1ba717179ad615f8a853d46cadeb51f6cb2cdd41
Mike Still <mikestill12@gmail.com>
2022-06-10 18:43:24 -0400

    Added inventory, file reader classes

---
+++ src/main/java/com/techelevator/model/Inventory.java
@@ -0,0 +1,10 @@
+package com.techelevator.model;
+
+public class Inventory {
+}
+
+// start inventory for each item at 5
+
+// decrease individual quantity when customer purchases an item
+
+// reset quantity to default when vending machine starts again
