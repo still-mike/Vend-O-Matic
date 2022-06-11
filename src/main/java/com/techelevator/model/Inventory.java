3bfdfb0fb83a2f0a5fa02abb9791b85a22018e39
Joelle Clayton <joelleclayton@mac.com>
2022-06-10 20:57:47 -0400

    submitting changes

--- src/main/java/com/techelevator/model/Inventory.java
+++ src/main/java/com/techelevator/model/Inventory.java
@@ -1,6 +1,26 @@
 package com.techelevator.model;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
 public class Inventory {
+
+    Map<Item, Integer> inventoryList = new TreeMap<>();
+
+    String fileString = "vendingmachine.csv";
+    File inventoryFile = new File(fileString);
+    Scanner fileScanner = new Scanner(inventoryFile);
+
+
+    public Inventory () throws FileNotFoundException {
+
+    }
+
+
+
 }
 
 // start inventory for each item at 5
---
+++ src/main/java/com/techelevator/model/fileScanner.java
@@ -0,0 +1,15 @@
+package com.techelevator.model;
+
+import java.io.File;
+
+public class FileScanner {
+    String fileString = "vendingmachine.csv";
+    private File inventoryFile;
+
+    public FileScanner(File inventoryFile) {
+
+
+    }
+}
+
+
